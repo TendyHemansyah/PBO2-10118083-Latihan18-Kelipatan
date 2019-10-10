e690daa318ad1c25985e70f31ef019eed023cbe9
Tendy Hermansyah <55569231+TendyHemansyah@users.noreply.github.com>
2019-10-10 07:58:31 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan18/kelipatan/PBO210118083Latihan18Kelipatan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan18.kelipatan;
+
+/**
+ *
+ * @author Tendy
+ * Nama     : Tendy Hermansyah
+ * Kelas    : PBO2
+ * Nim      : 10118083
+ * Deskripsi Program : Kelipatan
+ */
+public class PBO210118083Latihan18Kelipatan {
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i += 3.5){
+            System.out.println(i);
+        }
+    }
+}
